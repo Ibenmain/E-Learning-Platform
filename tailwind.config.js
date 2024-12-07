1d660d640abc4202303ab5d5283ced44843cc499
ibenmain <issambenmaina92@gmail.com>
2024-12-07 20:55:26 +0100

    feat: change the config tailwind to js ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 import fluid, { extract } from 'fluid-tailwind';
 
 module.exports = {
   content: {
     files: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
     ],
     extract,
   },
