740e4a53bef61e8f2d61f83b91697b13dde46d12
ibenmain <issambenmaina92@gmail.com>
2024-12-10 17:07:31 +0100

    refactore: herosection button responsive

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -11,10 +11,10 @@ const HeroSection = () => {
                 <h1 className='~text-[20px]/[47px] ~leading-[20px]/[48px] w-full font-extrabold'>Master Robotic AI, 3D CAD, and Coding</h1>
                 <p className='~text-[15px]/[20px] ~leading-[16px]/[28px] text-[#CBD5E1] text-center font-semibold'>Unlock your potential with our free learning platform. Dive into the  world of robotics, artificial intelligence, 3D modeling, and  programming.</p>
                 <div className='flex gap-4  font-medium'>
-                    <button className='rounded-[4px] h-10 w-32 font-sans  bg-gradient-to-tr from-[#9FEF00] to-[#03FF89] text-black'>
+                    <button className='rounded-[4px] h-8 sm:h-10 w-24 sm:w-32 font-sans text-xs sm:text-base  bg-gradient-to-tr from-[#9FEF00] to-[#03FF89] text-black'>
                         Get started
                     </button>
-                    <button className='rounded-[4px]  h-10 w-32 font-sans bg-black text-white px-7 '>
+                    <button className='rounded-[4px] h-8 sm:h-10 w-24 sm:w-32 font-sans text-xs sm:text-base bg-black text-white px-5 sm:px-7 '>
                         More info
                         <div className='h-[1px]  bg-white w-full'></div>
                     </button>
